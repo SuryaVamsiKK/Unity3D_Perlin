e23736fcd655b847ee3e8610ac50fee8273964ba
Surya Vamsi <kksvamsi@github.com>
2018-06-23 17:51:22 +0530

    Line Visualization added to try differnt methods on verts

---
+++ Perlin3D/Perlin3D/Assets/Project/Scripts/LineVisualizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineVisualizer : MonoBehaviour {
+
+	public Color linesColor;
+	public Color vertsColor;
+	public float radius;
+	public Transform[] verts;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = vertsColor;
+		Gizmos.DrawWireSphere(this.transform.position, radius);
+		for (int i = 0; i < verts.Length; i++)
+		{ 
+			Gizmos.color = linesColor;
+			Gizmos.DrawLine(this.transform.position, verts[i].transform.position);
+		}
+	}
+}
