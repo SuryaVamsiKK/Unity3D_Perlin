0b2fe664f973a49bc20377703b81e9a3774405bc
Surya Vamsi <kksvamsi@github.com>
2018-06-26 02:52:21 +0530

    A Complete Implimentation of wokring marching cube

---
+++ Perlin3D/Perlin3D/Assets/Project/Scripts/ColorCode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCode : MonoBehaviour {
+
+	public Color MeshColor;
+	public Color vertOffColor;
+	public Color vertOnColor;
+	public Color edgeOffColor;
+	public Color edgeOnColor;
+	public Color linecolor;
+	public float vertSize;
+	public float edgeSize;
+}
