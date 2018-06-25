0acac57f66608c8517a370493a2041f4d741a72e
Surya Vamsi <kksvamsi@github.com>
2018-06-25 17:43:59 +0530

    Finally Woking

--- Perlin3D/Perlin3D/Assets/Project/Scripts/Visualizer.cs
+++ Perlin3D/Perlin3D/Assets/Project/Scripts/Visualizer.cs
@@ -13,11 +13,50 @@ public class Visualizer : MonoBehaviour {
 	public Color linecolor;
 	public float vertSize;
 	public float edgeSize;
-	public bool[] verts;
-	public bool[] e;
+	bool[] e;
 	public bool[] v;
 
-	int[,] combs =
+	//                 v7_______e6_____________v6
+	//                  /|                    /|
+	//                 / |                   / |
+	//              e7/  |                e5/  |
+	//               /___|______e4_________/   |
+	//            v4|    |                 |v5 |e10
+	//              |    |                 |   |
+	//              |    |e11              |e9 |
+	//            e8|    |                 |   |
+	//              |    |_________________|___|
+	//              |   / v3      e2       |   /v2
+	//              |  /                   |  /
+	//              | /e3                  | /e1
+	//              |/_____________________|/
+	//              v0         e0          v1
+
+
+	//				All the neightbouring edges
+	//				v0  -----  e0		e3		e8
+	//				v1  -----  e0		e1		e9
+	//				v2  -----  e1		e10		e2
+	//				v3  -----  e2		e3		e11
+	//				v4  -----  e8		e7		e4
+	//				v5  -----  e4		e5		e9
+	//				v6  -----  e5		e6		e10
+	//				v7  -----  e6		e7		e11
+
+	//				All the neightbouring Verts
+	//				v0  -----  v1		v3		v4
+	//				v1  -----  v0		v2		v5
+	//				v2  -----  v1		v6		v3
+	//				v3  -----  v2		v0		v7
+	//				v4  -----  v0		v7		v5
+	//				v5  -----  v4		v6		v1
+	//				v6  -----  v5		v7		v2
+	//				v7  -----  v6		v4		v3
+
+
+	#region Neighbour Tables
+
+	int[,] edgeNeighbours =
 		{ { 0, 3, 8},
 		  { 0, 1, 9},
 		  { 1, 10, 2},
@@ -28,23 +67,23 @@ public class Visualizer : MonoBehaviour {
 		  { 6, 7, 11},
 		};
 
-	int[,] neighbours =
-		{ { 1, 0, 3, 3, 4, 8},
-		  { 0, 0, 2, 1, 5, 9},
-		  { 1, 1, 6, 10, 3, 2},
-		  { 0, 3, 2, 2, 7, 11},
-		  { 0, 8, 5, 4, 7, 7},
-		  { 1, 9, 6, 5, 4, 4},
-		  { 5, 5, 7, 6, 2, 10},
-		  { 6, 7, 3, 11, 6, 6},
+	int[,] vertNeighbours =
+		{ { 1, 3, 4},
+		  { 0, 2, 5},
+		  { 1, 6, 3},
+		  { 2, 0, 7},
+		  { 0, 7, 5},
+		  { 4, 6, 1},
+		  { 5, 7, 2},
+		  { 6, 4, 3},
 		};
 
-	// Use this for initialization
+	#endregion
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -56,7 +95,7 @@ public class Visualizer : MonoBehaviour {
 
 	private void OnDrawGizmos()
 	{
-		combinations();
+		Evaluation();
 		if (this.transform.childCount >= 2)
 		{
 			#region Verts and Mids
@@ -113,87 +152,72 @@ public class Visualizer : MonoBehaviour {
 		Gizmos.DrawLine(p1.transform.position, p2.transform.position);
 	}
 
-	void combinations()
+	void Evaluation()
 	{
-
-		//v0  -----  e0		e3		e8
-		//v1  -----  e0		e1		e9
-		//v2  -----  e1		e10		e2
-		//v3  -----  e2		e3		e11
-		//v4  -----  e8		e7		e4
-		//v5  -----  e4		e5		e9
-		//v6  -----  e5		e6		e10
-		//v7  -----  e6		e7		e11
-
-		//enabler(0, 0, 3, 8);
-		//enabler(1, 0, 1, 9);
-		//enabler(2, 1, 10, 2);
-		//enabler(3, 2, 3, 11);
-		//enabler(4, 8, 7, 4);
-		//enabler(5, 4, 5, 9);
-		//enabler(6, 5, 6, 10);
-		//enabler(7, 6, 7, 11);
-		#region trial one
 		for (int i = 0; i < 8; i++)
 		{
-			if (verts[i] == true)
-			{
-				v[i] = !v[i];
-				enabler(i, combs[i, 0], combs[i, 1], combs[i, 2]);
-				verts[i] = false;
-			}
+			Evaluate_Boolean(i, edgeNeighbours[i, 0], edgeNeighbours[i, 1], edgeNeighbours[i, 2]);
 		}
-		#endregion
 	}
 
-	void enabler(int vert, int e1, int e2, int e3)
+	void Evaluate_Boolean(int vert, int e1, int e2, int e3)
 	{
-		//                 v7_______e6_____________v6
-		//                  /|                    /|
-		//                 / |                   / |
-		//              e7/  |                e5/  |
-		//               /___|______e4_________/   |
-		//            v4|    |                 |v5 |e10
-		//              |    |                 |   |
-		//              |    |e11              |e9 |
-		//            e8|    |                 |   |
-		//              |    |_________________|___|
-		//              |   / v3      e2       |   /v2
-		//              |  /                   |  /
-		//              | /e3                  | /e1
-		//              |/_____________________|/
-		//              v0         e0          v1
-
 		if (v[vert] == true)
 		{
-			e[e1] = !e[e1];
-			e[e2] = !e[e2];
-			e[e3] = !e[e3];
-		}
-		if (v[vert] == false)
-		{
-			e[e1] = false;
-			e[e2] = false;
-			e[e3] = false;
+			// Firstly making all the linked edges true.
+			e[e1] = true;
+			e[e2] = true;
+			e[e3] = true;
 
+			//Checking what neighbours are ON and disabling the edges based on the Neighbours that are ON.
 			for (int i = 0; i < 8; i++)
 			{
 				if (vert == i)
 				{
-					if (v[neighbours[i, 0]] == true)
+					if (v[vertNeighbours[i, 0]] == true)				// if the neighbour is true then siwtch the link betwwen them off
 					{
-						e[neighbours[i, 1]] = true;
+						e[edgeNeighbours[i, 0]] = false;
 					}
-					if (v[neighbours[i, 2]] == true)
+					if (v[vertNeighbours[i, 1]] == true)
 					{
-						e[neighbours[i, 3]] = true;
+						e[edgeNeighbours[i, 1]] = false;
 					}
-					if (v[neighbours[i, 4]] == true)
+					if (v[vertNeighbours[i, 2]] == true)
 					{
-						e[neighbours[i, 5]] = true;
+						e[edgeNeighbours[i, 2]] = false;
 					}
 				}
 			}
+			//checking the neighbours based on the integer table above just to reduce the some many if statments.
+		}
+
+		if (v[vert] == false)
+		{
+			// Firstly making all the linked edges false.
+			e[e1] = false;
+			e[e2] = false;
+			e[e3] = false;
+
+			//Checking what neighbours are ON and leaving the linked edges that connect to the neighbours ON.
+			for (int i = 0; i < 8; i++)
+			{
+				if (vert == i)
+				{
+					if (v[vertNeighbours[i, 0]] == true)        // if the neighbour is true then siwtch the link betwwen them back on
+					{
+						e[edgeNeighbours[i, 0]] = true;
+					}
+					if (v[vertNeighbours[i, 1]] == true)
+					{
+						e[edgeNeighbours[i, 1]] = true;
+					}
+					if (v[vertNeighbours[i, 2]] == true)
+					{
+						e[edgeNeighbours[i, 2]] = true;
+					}
+				}
+			}
+			//again checking the neighbours based on the integer table above just to reduce the some many if statments.
 		}
 	}
 }
